b3fe009837a673e81f31f245ca9bd2d22dbe1a7b
tardigrada777 <ntwrite@gmail.com>
2019-09-09 19:12:49 +0300

    Подготовка к деплою 2

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ server.use('/api', jsonServer.router('db.json'));
 const port = process.env.PORT || 3000;
 
 server.set('port', port);
+
+server.use(express.static('dist'));
+
+server.get('/', (req, res) => {
+  res.render('index.html');
+});
+
 server.listen(port, () => {
   console.log(`Server running on ${port}`);
 });
